12363195a994f94968561f21f386628fdd3235fd
Mic Hussey <Michael.Hussey@solace.com>
2017-10-10 22:39:34 +0200

    Used property to paramaterise QueueName, generalised processMsg

--- solace-jms-sample-app/src/main/java/demo/DemoApplication.java
+++ solace-jms-sample-app/src/main/java/demo/DemoApplication.java
@@ -1,20 +1,24 @@
 package demo;
 
+import java.util.Iterator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class DemoApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
@@ -26,24 +30,39 @@ public class DemoApplication {
 
         @Autowired
         private JmsTemplate jmsTemplate;
+        
+        @Value("${solace.jms.demoQueueName}")
+        private String queueName;
 
         @Override
         public void run(String... strings) throws Exception {
             String msg = "Hello World";
             logger.info("============= Sending " + msg);
-            this.jmsTemplate.convertAndSend("testQueue", msg);
+            this.jmsTemplate.convertAndSend(queueName, msg);
         }
     }
 
     @Component
     static class MessageHandler {
-
+ 
         private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
 
-        @JmsListener(destination = "testQueue")
-        public void processMsg(String msg) {
-            logger.info("============= Received " + msg);
+        // Retrieve the name of the queue from the application.properties file
+        @JmsListener(destination = "${solace.jms.demoQueueName}")
+        public void processMsg(Message msg) {
+        	StringBuffer msgAsStr = new StringBuffer("============= Received \nHeaders:");
+        	MessageHeaders hdrs = msg.getHeaders();
+        	msgAsStr.append("\nUUID: "+hdrs.getId());
+        	msgAsStr.append("\nTimestamp: "+hdrs.getTimestamp());
+        	Iterator<String> keyIter = hdrs.keySet().iterator();
+        	while (keyIter.hasNext()) {
+        		String key = keyIter.next();
+            	msgAsStr.append("\n"+key+": "+hdrs.get(key));        		
+        	}
+        	msgAsStr.append("\nPayload: "+msg.getPayload());
+            logger.info(msgAsStr.toString());
         }
     }
+
 
 }
